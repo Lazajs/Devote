d19e68ebddb1b490e37397c0688410dbc927a009
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-02-01 21:31:07 -0300

    Add: DisplayName component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import topBg from 'public/images/top-bg.svg'
 import bottomBg from 'public/images/bottom-bg.svg'
 import { Poppins } from '@next/font/google'
-import UserLoggedProvider from '@/Providers/UserLoggedProvider'
+import {SessionProvider} from 'next-auth/react'
 
 const poppins = Poppins({subsets: ['latin'], weight: ['400']})
 
@@ -16,11 +16,11 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={`bg-background w-screen h-screen ${poppins.className}`}>
-        <UserLoggedProvider>
           <main className='max-w-7xl block m-auto w-11/12 relative'>
-            {children}
+            <SessionProvider>
+              {children}
+            </SessionProvider>
           </main>
-        </UserLoggedProvider>
         <Image priority src={bottomBg} className="absolute top-0 right-0 min-w-full max-h-40 select-none z-0 rotate-180" alt='background' />
         <Image priority className="absolute bottom-0 min-w-full max-h-96 select-none z-0 lg:max-h-40 sm:max-h-28" src={topBg} alt='background' />
         </body>
