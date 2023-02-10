480c0ac9cb26dd77212069690209b1275c115669
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-02-09 23:21:56 -0300

    fix: Auth user with Product model

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,10 @@ import { MongooseAdapter } from "@choutkamartin/mongoose-adapter";
 import Credentials from "next-auth/providers/credentials";
 import type {SignData, ProductData} from 'types'
 import GithubProvider from 'next-auth/providers/github'
-// import dbConnect from "@/db/mongoose";
+import dbConnect from "@/db/mongoose";
+import Product from "@/db/models/product";
+import User from "@/db/models/user";
+import { signIn } from "next-auth/react";
 
 interface WithPath extends SignData {
   from: 'signin' | 'signup'
@@ -15,13 +18,13 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientSecret: process.env.GITHUB_SECRET as string
     }),
     Credentials({
       name: 'Credentials',
       credentials: {
         username: { label: 'Username', type: 'text', placeholder:'John Smith'},
-        password: {label: 'Password', type: 'password', placeholder:'mYpaSSword2@f'}
+        password: {label: 'Password', type: 'password', placeholder:'mYpaSSword2@f'},
       },
 
       async authorize(credentials, _req) {
@@ -53,10 +56,12 @@ export const authOptions: NextAuthOptions = {
    callbacks: {
     async session({ session, token }) {
       session.user = token.user as ProductData;
+      // console.log(token.user) work here
       return session;
     },
     async jwt({ token, user }) {
       if (user) {
+        console.log(user)
         token.user = user;
       }
       return token;
